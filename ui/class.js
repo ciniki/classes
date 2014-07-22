d6c82c635aa948375ce459799f93577e8509e1de
Andrew Rivett <veggiefrog@gmail.com>
2014-07-22 16:10:25 -0400

    Moved manage-themes into core

--- ui/class.js
+++ ui/class.js
@@ -139,7 +139,7 @@ function ciniki_classes_class() {
 			if( d.image.image_data != null && d.image.image_data != '' ) {
 				return d.image.image_data;
 			} else {
-				return '/ciniki-manage-themes/default/img/noimage_75.jpg';
+				return '/ciniki-mods/core/ui/themes/default/img/noimage_75.jpg';
 			}
 		};
 		this.edit.thumbTitle = function(s, i, d) {
